fb0c98f84eba4bafacbc94c71684d432b8b4d5a8
tabarak23 <er.tabarak2000@gmail.com>
2026-03-03 09:33:10 +0530

    added destroy to delete pending kms keys

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-vprofile-81067"
+    bucket = "terraform-state-vprofile-810674"
     key    = "dev/terraform.tfstate"
     region = "us-west-1"
   }
